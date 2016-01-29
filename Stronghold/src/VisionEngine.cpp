8da720fa773b79245d6edc8b99b54771ca9a6dc7
Mr. Noll <GEH-Magnet School 1@GHSRobotics05>
2016-01-29 18:38:42 -0500

    Create VisionEngine class

---
+++ Stronghold/src/VisionEngine.cpp
@@ -0,0 +1,18 @@
+/*
+ * VisionEngine.cpp
+ *
+ *  Created on: Jan 29, 2016
+ *      Author: GEH-Magnet School 1
+ */
+
+#include "VisionEngine.h"
+
+VisionEngine::VisionEngine() {
+	// TODO Auto-generated constructor stub
+
+}
+
+VisionEngine::~VisionEngine() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Stronghold/src/VisionEngine.h
@@ -0,0 +1,17 @@
+/*
+ * VisionEngine.h
+ *
+ *  Created on: Jan 29, 2016
+ *      Author: GEH-Magnet School 1
+ */
+
+#ifndef SRC_VISIONENGINE_H_
+#define SRC_VISIONENGINE_H_
+
+class VisionEngine {
+public:
+	VisionEngine();
+	virtual ~VisionEngine();
+};
+
+#endif /* SRC_VISIONENGINE_H_ */
