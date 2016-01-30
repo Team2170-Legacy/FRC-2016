387b45c9d5b429c1d64838398d0996b5aab72942
Tushar <tusharsondhiepic@gmail.com>
2016-01-29 19:43:36 -0500

    Stubbed out vision engine

--- Stronghold/src/VisionEngine.cpp
+++ Stronghold/src/VisionEngine.cpp
@@ -6,9 +6,12 @@
  */
 
 #include "VisionEngine.h"
+#include <unistd.h>
+#include <stdio.h>
 
 VisionEngine::VisionEngine() {
 	// TODO Auto-generated constructor stub
+	table = NetworkTable::GetTable("GRIP/myContoursReport");
 
 }
 
@@ -16,3 +19,23 @@ VisionEngine::~VisionEngine() {
 	// TODO Auto-generated destructor stub
 }
 
+void VisionEngine::ProcessContours() {
+	//GRIP Network Table
+		std::cout << "Areas: ";
+		//get the array with an empty array as the default value
+		std::vector<double> arr = table->GetNumberArray("area", llvm::ArrayRef<double>());
+		for (unsigned int i = 0; i < arr.size(); i++) {
+			std::cout << arr[i] << " ";
+		}
+		std::cout << std::endl;
+
+}
+
+void VisionEngine::StartGRIP() {
+	/* Run GRIP in a new process */
+	if (fork() == 0) {
+		if (execv(JAVA, GRIP_ARGS) == -1) {
+			perror("Error running GRIP");
+		}
+	}
+}
--- Stronghold/src/VisionEngine.h
+++ Stronghold/src/VisionEngine.h
@@ -7,11 +7,24 @@
 
 #ifndef SRC_VISIONENGINE_H_
 #define SRC_VISIONENGINE_H_
+#include "WPILib.h"
 
 class VisionEngine {
+private:
+	//Grip Network Table
+	std::shared_ptr<NetworkTable> table;
+
+	const char *JAVA = "/usr/local/frc/JRE/bin/java";
+	char *GRIP_ARGS[5] = { "java", "-jar", "/home/lvuser/grip.jar",
+			"/home/lvuser/project.grip", NULL };
+
+
+
 public:
 	VisionEngine();
 	virtual ~VisionEngine();
+	void ProcessContours();
+	void StartGRIP();
 };
 
 #endif /* SRC_VISIONENGINE_H_ */
