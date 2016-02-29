34a4a6e4604ff123255829ede95521f982cfdf54
Mr. Noll <GEH-Magnet School 1@GHSRobotics05>
2016-02-29 18:58:38 -0500

    Added command to set talon mode

--- Stronghold/src/OI.cpp
+++ Stronghold/src/OI.cpp
@@ -34,6 +34,7 @@
 #include "Commands/ChassisRotate180CW.h"
 #include "Commands/ChassisRotate90CCW.h"
 #include "Commands/ChassisRotate90CW.h"
+#include "Commands/ChassisSetMode.h"
 #include "Commands/ShooterFlywheelHigh.h"
 #include "Commands/ShooterFlywheelLow.h"
 #include "Commands/ShooterFlywheelReverse.h"
