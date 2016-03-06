ab813d75afcfddeb301651c27bc8e4cac2f439be
Mr. Noll <GEH-Magnet School 1@GHSRobotics05>
2016-03-05 21:58:27 -0500

    created slower autonomous drive under bar

--- Stronghold/src/ProfileData.h
+++ Stronghold/src/ProfileData.h
@@ -15,20 +15,31 @@ extern const ProfileData TestData2;
 
 // 45 degrees clockwise
 extern const ProfileData Rotate45Clockwise;
+
 // 90 degrees clockwise
 extern const ProfileData Rotate90Clockwise;
+
 // 180 degrees clockwise
 extern const ProfileData Rotate180Clockwise;
+
 // 45 degrees counterclockwise
 extern const ProfileData Rotate45CounterClockwise;
+
 // 90 degrees counterclockwise
 extern const ProfileData Rotate90CounterClockwise;
+
 // 180 degrees counterclockwise
 extern const ProfileData Rotate180CounterClockwise;
+
 // 50 inches forward
 extern const ProfileData AutonomousMoveToDefense;
+
 // 230 inches forward, past the low bar
 extern const ProfileData AutonomousDrivePastLowBar;
+
+// 100  inches, 50 ips
+extern const ProfileData AutonomousDrivePastLowBar_100;
+
 // 60 degrees clockwise
 extern const ProfileData Rotate60Clockwise;
 
